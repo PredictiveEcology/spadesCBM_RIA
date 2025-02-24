7c80f2a386bb07e8b8ebcc0d240d6c5663f76b3c
camillegiuliano <camille.giuliano@nrcan-rncan.gc.ca>
2025-02-24 12:05:08 -0800

    adding R project and global skeleton

---
+++ globalRIApresentday.R
@@ -0,0 +1,5 @@
+projectPath <- "~/GitHub/spadesCBM_RIA"
+repos <- unique(c("predictiveecology.r-universe.dev", getOption("repos")))
+install.packages("SpaDES.project",
+                 repos = repos)
+
