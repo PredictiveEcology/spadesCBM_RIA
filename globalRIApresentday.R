c88e9dcba9269d286c4ed11ba2ee057c3a3db5eb
camillegiuliano <camille.giuliano@nrcan-rncan.gc.ca>
2025-04-15 12:57:05 -0700

    removes python environment creation from global

--- globalRIApresentday.R
+++ globalRIApresentday.R
@@ -3,13 +3,18 @@ repos <- unique(c("predictiveecology.r-universe.dev", getOption("repos")))
 install.packages("SpaDES.project",
                  repos = repos)
 
+
 times <- list(start = 2015, end = 2015) ##TODO: This is from the SK runs, set times needed. 
 
 out <- SpaDES.project::setupProject(
   Restart = TRUE,
   useGit = "PredictiveEcology", # a developer sets and keeps this = TRUE
   overwrite = TRUE, # a user who wants to get latest modules sets this to TRUE
-  paths = list(projectPath = projectPath),
+  paths = list(projectPath = projectPath,
+               modulePath  = file.path(projectPath, "modules"),
+               inputPath   = file.path(projectPath, "inputs"),
+               cachePath   = file.path(projectPath, "cache"),
+               outputPath  = file.path(projectPath, "outputs")),
   
   options = options(
     repos = c(repos = repos),
@@ -23,7 +28,8 @@ out <- SpaDES.project::setupProject(
   modules =  c("PredictiveEcology/CBM_defaults@development",
                "PredictiveEcology/CBM_dataPrep_RIA@presentDay",
                "PredictiveEcology/CBM_vol2biomass_RIA@development",
-               "PredictiveEcology/CBM_core@development"),
+               "PredictiveEcology/CBM_core@development"
+               ),
   times = times,
   require = c("SpaDES.core", "reticulate",
               "PredictiveEcology/libcbmr", "data.table", "googledrive"),
@@ -39,32 +45,6 @@ out <- SpaDES.project::setupProject(
       .useCache = TRUE
     )
   ),
-  functions = "PredictiveEcology/CBM_core@training/R/ReticulateFindPython.R",
-  
-  ret = {
-    reticulate::virtualenv_create(
-      "r-spadesCBM",
-      python = if (!reticulate::virtualenv_exists("r-spadesCBM")){
-        ReticulateFindPython(
-          version        = ">=3.9,<=3.12.7",
-          versionInstall = "3.10:latest",
-          pyenvRoot      = tools::R_user_dir("r-spadesCBM")
-        )
-      },
-      packages = c(
-        "numpy<2",
-        "pandas>=1.1.5",
-        "scipy",
-        "numexpr>=2.8.7",
-        "numba",
-        "pyyaml",
-        "mock",
-        "openpyxl",
-        "libcbm"
-      )
-    )
-    reticulate::use_virtualenv("r-spadesCBM")
-  },
   
   #### begin manually passed inputs #########################################
 
