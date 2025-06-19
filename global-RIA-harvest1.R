df70adce0ccfc333db77cc46046968276ee18640
Susan Murray <murray.e.susan@gmail.com>
2025-06-19 08:17:14 -0700

    times fix

--- global-RIA-harvest1.R
+++ global-RIA-harvest1.R
@@ -14,7 +14,7 @@ if (tryCatch(packageVersion("SpaDES.project") < "0.1.1", error = function(x) TRU
 }
 
 # Set simulation time span
-times <- list(start = 2020, end = 2100)
+times <- list(start = 2020, end = 2099)
 
 # Set up project
 out <- SpaDES.project::setupProject(
